bc08d312f35fde5451b633f20a7bbc1e23bcca8e
Henrik Lievonen <henrik.lievonen@hotmail.com>
2018-01-17 00:35:39 +0200

    Create overlays based on users.csv

--- util/create-overlays.sh
+++ util/create-overlays.sh
@@ -12,19 +12,20 @@ fi
 userlist=$1
 shift
 
-userlist="$(cat "$userlist")"
+userlist="$(cat "$userlist" | cut -d'"' -f 6)"
 
 font="$(fc-match -f '%{file}' Monospace)"
 text_options="-pointsize 32 -fill white -channel RGBA -font "$font""
 background="-define gradient:vector=300,0,350,0 gradient:black-none"
 
+idx=1
 while read user; do
     convert \
         -size 400x50 \
-        -delay 100 -loop 0 \
-        \( $background $text_options -draw "text 0,30 '$user'" \) \
         \( $background $text_options -draw "text 0,30 '$user|'" \) \
         -layers Optimize \
-        foo.gif
+        "user-overlays/user-$i.png"
+
+    i=$((i+1))
 done <<< "$userlist"
 
